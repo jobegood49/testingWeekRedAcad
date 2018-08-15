76e86bdb883a26c085e375433b523aa858a66af2
jonathannicolas <jonathan.s.nicolas@gmail.com>
2018-08-15 10:13:35 -0700

    Add 5 test fails for oneEditAway

---
+++ __tests__/oneEditAway.spec.js
@@ -0,0 +1,40 @@
+
+
+const oneEditAway = require("../lib/oneEditAway.js");
+
+describe("oneEditAway", () => {
+  describe("when one letter was deleted", () => {
+    it("should return true", () => {
+      const result = oneEditAway("pale", "ple");
+      expect(result).toEqual(true);
+    });
+  });
+
+  describe("when one letter was deleted", () => {
+    it("should return true", () => {
+      const result = oneEditAway("pales", "pale");
+      expect(result).toEqual(true);
+    });
+  });
+
+  describe("when one letter was edited", () => {
+    it("should return true", () => {
+      const result = oneEditAway("pale", "bale");
+      expect(result).toEqual(true);
+    });
+  });
+
+  describe("when more than one letter was edited", () => {
+    it("should return false", () => {
+      const result = oneEditAway("pale", "bake");
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe("when more than one letter was edited and added", () => {
+    it("should return false", () => {
+      const result = oneEditAway("face", "facts");
+      expect(result).toEqual(false);
+    });
+  });
+});
